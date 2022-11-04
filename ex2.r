def7eae501fd7bd21625250ab0d3dbb8881af367
RickyJ99 <riccardodalcero99@gmail.com>
2022-11-04 11:15:34 +0100

    ex2 adding functions

--- ex2.r
+++ ex2.r
@@ -1,25 +1,110 @@
-max <- 1.054
-min <- 0.982
-half <- (max-min)/2
-m <- c((max-min)/4,(max-min)*3/4)
-i <- 0 
-while(i<1000){
-    bis <- matrix(NA, nrow(2), ncol(4))
-    #   index    m   free    p   val
-    #   1
-    #   2
-    #recall a function to compute risk rate for m[1] and the index
-    bis[1,] <- function(m)
-    #recall a function to compute risk rate for m[1] and the index
-    bis[2,] <- function(m)
-    #select the one with max index 
-    if( bis[1,3]>=bis[2,3]){
-        max <- half
-        half <- (max-min)/2
-        i <- bis[1,3]
-    } else {
-       min <- half
-       half <- (max-min)/2
-       i <- bis[2,3]
+library(matlib)
+v <- function(dummy, x) {
+  for (t in 1 : length(dummy)) {
+    if (dummy[t] > 4) {
+      dummy[t]  <-  1
+    }
+    else {
+      dummy[t]  <-  0
+    }
+  }
+  value <-  abs(1 / (1 - x)) * dummy
+  return(value)
+}
+riskComputation <- function(m){
+    #parameters 
+    phi     <- 0.43
+    beta    <- 0.96
+    gamma   <- 2
+    h       <- 1.054
+    l       <- 0.982
+    epsilon <- 0.012
+
+    valuesPi        <-   c(phi, epsilon, 1 - phi - epsilon, 1 / 2,
+     0, 1 / 2, 1 - phi - epsilon, epsilon, phi)
+    Pi              <-   matrix(valuesPi, byrow = TRUE, nrow = 3, ncol = 3)
+    Pi
+
+
+    values_states   <-   c(h,m,l)
+
+    #ouput vector
+    out <-  c()
+
+    #stationary prob.
+    st_prob <- c(0.5, epsilon, 0.5) * (1 + epsilon)
+    st_prob
+
+    #ZCB Price 
+
+    q   <-  beta * (Pi %*% (values_states ^- gamma))
+    q
+
+    #risk-free rate vector
+    risk_free   <-   (1 / (q)) - 1
+    risk_free_h <-   risk_free[1, ]
+    risk_free_m <-   risk_free[2, ]
+    risk_free_l <-   risk_free[3, ]
+
+    #mean risk free rate
+    rf <- risk_free[, 1] %*% st_prob
+    as.numeric(rf)
+
+    #risky_rate
+    values_matrix <- diag(3) * (values_states^(1 - gamma))
+
+
+    Pi_star <-  Pi %*% values_matrix
+    Pi_star
+    I       <- diag(3)
+    v1      <- c(1, 1, 1)
+
+    p       <- (t(I - beta * Pi_star)) %*% (beta * (Pi_star %*% v1))
+    p
+
+    rp      <- p[, 1] %*% st_prob
+    as.numeric(rp)
+
+    #compute index
+    i       <- v(rp, rf)
+
+    out     <- c(rf, rp, i)
+
+    return(out)
+
+}
+
+main <- function() {
+    #bisection variable
+    max     <- 1.054
+    min     <- 0.982
+    half    <- (max - min) / 2 + min
+    i       <- 0
+
+    while(i < 1){
+        m1  <- c((max - min) / 4, (max - min) * 3 / 4) + min
+        print(m1)
+        bis <- matrix(NA, nrow = 2, ncol = 4)
+        #   index    m   free    p   val
+        #   1
+        #   2
+        bis[, 1] <- as.numeric(m1)
+        #recall a function to compute risk rate for m[1] and the index
+        bis[1, 2:4] <- riskComputation(m1[1])
+
+        #recall a function to compute risk rate for m[1] and the index
+        bis[2, 2:4] <- riskComputation(m1[2])
+        print(bis)
+        #select the one with max index
+        if (bis[1, 4] >= bis[2, 4]) {
+            max     <- half
+            half    <- (max - min) / 2 + min
+            i       <- bis[1, 4]
+        } else {
+            min     <- half
+            half    <- (max - min) / 2 + min
+            i       <- bis[2, 4]
+        }
     }
 }
+main()
